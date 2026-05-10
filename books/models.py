ffc5337c6c3f64a282163cb30d09c9102e64b34f
Bipin Thapa <tailung12345678910@gmail.com>
2026-05-10 21:08:51 +0545

    library website

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(250)
+    subtitle = models.CharField(250)
+    author = models.CharField(100)
+    isbn = models.CharField(10)
+
+    def __str__(self):
+        return self.title
